331312b4d00528a1913ce6bac91b017198ab3fa0
ekrausso <ekrauss@eclipsoft.com>
2023-01-09 17:42:38 -0500

    menu colapsable responsive

---
+++ src/pages/posts-api.ts
@@ -0,0 +1,22 @@
+export async function get({params, request}) {
+
+    console.log(params, request)
+    const id = params.id
+
+    const data = await (await fetch(`https://jsonplaceholder.typicode.com/posts`)).json();
+
+    return {
+        body: JSON.stringify({
+            data: data
+        })
+    }
+}
+
+
+// export function getStaticPaths() {
+//     return [
+//         {params: {id: '1'}},
+//         {params: {id: '2'}},
+//         {params: {id: '3'}},
+//     ]
+// }
